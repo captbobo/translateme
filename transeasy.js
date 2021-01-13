7e80502f5170c51b188f4d6596469275cca5471e
captbobo <cansurmeli@gmail.com>
2021-01-13 21:39:23 +0300

    feat: create Blob and download

--- transeasy.js
+++ transeasy.js
@@ -5,6 +5,17 @@ while (!tableRows[0].className) {
   // because since the first is shifted...
 }
 
+const tasks = tableRows.map( elem => {
+  const task = new Task(elem)
+  task.setTargetText()
+  return task
+});
+
+// uncomment to activate
+// download(tasks)
+
+
+
 const getID = (elem) => elem.getElementsByTagName("strong")[1].innerText.split(' ')[2]
 const getSource = (id) => document.getElementById('text_en_'+ id).innerText
 const getPreviousTranslation = (elem) =>   {
@@ -28,8 +39,13 @@ function Task(elem) {
   }
 }
 
-const tasks = tableRows.map( elem => {
-  const task = new Task(elem)
-  task.setTargetText()
-  return task
-});
+// creates and downloads pretty printed JSON of data 
+function download(object) {
+  const array = [JSON.stringify(object, null, 2)]
+  const blob = new Blob(array, {type: 'text/plain; charset=utf-8'})
+  const link = document.createElement('a')
+  link.download = "translation-central.txt"
+  link.href =  URL.createObjectURL(blob)
+  link.click()
+  URL.revokeObjectURL(blob)
+  }
