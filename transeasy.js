da96c5eb7eba35d8df3b1a25ef013224cfc6bca0
captbobo <cansurmeli@gmail.com>
2021-01-13 22:12:27 +0300

    style

--- transeasy.js
+++ transeasy.js
@@ -2,7 +2,6 @@
 const tableRows = [...document.getElementsByTagName("tr")];
 while (!tableRows[0].className) {
   tableRows.shift();
-  // because since the first is shifted...
 }
 
 const tasks = tableRows.map( elem => {
@@ -14,15 +13,16 @@ const tasks = tableRows.map( elem => {
 // uncomment to activate
 // download(tasks)
 
-
-
 const getID = (elem) => elem.getElementsByTagName("strong")[1].innerText.split(' ')[2]
 const getSource = (id) => document.getElementById('text_en_'+ id).innerText
 const getPreviousTranslation = (elem) =>   {
   const i = elem.getElementsByTagName("pre")
   return i.length ? i.innerText : undefined
 }
+
 // for some reason, the translations served with
+// Machine Translated versions, stored as comments
+// inside the element
 const getComment = (elem) => {
   const comment = [...elem.childNodes].find( child => child.nodeType === 8 );
   return ((comment && comment.nodeValue) && comment.nodeValue[0] !== " ") ?
@@ -39,7 +39,7 @@ function Task(elem) {
   }
 }
 
-// creates and downloads pretty printed JSON of data 
+// creates and downloads pretty printed JSON of data
 function download(object) {
   const array = [JSON.stringify(object, null, 2)]
   const blob = new Blob(array, {type: 'text/plain; charset=utf-8'})
