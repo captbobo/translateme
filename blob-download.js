a78730693e91559101d16a9a95372e09ff02b1b5
captbobo <cansurmeli@gmail.com>
2021-03-18 18:05:40 +0300

    add readme template

--- blob-download.js
+++
@@ -1,9 +0,0 @@
-
-export const createBlob = object => {
-  const blob = new Blob(object, {type: 'text/plain; charset=utf-8'})
-  const link = document.createElement('a')
-  link.download = "translation-central.txt"
-  link.href =  URL.createObjectURL(blob)
-  link.click()
-  URL.revokeObjectURL(blob)
-  }
--- transeasy.js
+++ transeasy.js
@@ -1,4 +1,20 @@
 
+
+const getID = (elem) => elem.getElementsByTagName("strong")[1].innerText.split(' ')[2]
+const getSource = (id) => document.getElementById('text_en_'+ id).innerText
+const getPreviousTranslation = (elem) =>   {
+  const i = elem.getElementsByTagName("pre")
+  return i.length ? i.innerText : undefined
+}
+// for some reason, the translations served with
+// Machine Translated versions, stored as comments
+// inside the element
+const getComment = (elem) => {
+  const comment = [...elem.childNodes].find( child => child.nodeType === 8 );
+  return ((comment && comment.nodeValue) && comment.nodeValue[0] !== " ") ?
+    comment.nodeValue : undefined;
+}
+
 const tableRows = [...document.getElementsByTagName("tr")];
 while (!tableRows[0].className) {
   tableRows.shift();
@@ -12,22 +28,6 @@ const tasks = tableRows.map( elem => {
 
 // uncomment to activate
 // download(tasks)
-
-const getID = (elem) => elem.getElementsByTagName("strong")[1].innerText.split(' ')[2]
-const getSource = (id) => document.getElementById('text_en_'+ id).innerText
-const getPreviousTranslation = (elem) =>   {
-  const i = elem.getElementsByTagName("pre")
-  return i.length ? i.innerText : undefined
-}
-
-// for some reason, the translations served with
-// Machine Translated versions, stored as comments
-// inside the element
-const getComment = (elem) => {
-  const comment = [...elem.childNodes].find( child => child.nodeType === 8 );
-  return ((comment && comment.nodeValue) && comment.nodeValue[0] !== " ") ?
-    comment.nodeValue : undefined;
-}
 
 function Task(elem) {
   this.id = getID(elem),
